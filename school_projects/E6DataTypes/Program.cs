39202240c81f93a96cc2d9b6f0866fea84b07835
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-09-24 11:36:47 +0200

    Types project

---
+++ school_projects/E6DataTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string outputString = "Hello World";
+Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+
+
+
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
