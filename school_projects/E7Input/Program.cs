536155d66de5fc2c899a8a5b0f40b5edb2d742f7
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-09-24 11:59:27 +0200

    E7: Reading Input exercise

---
+++ school_projects/E7Input/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string outputString = "Output: What's your name?";
+Console.WriteLine(outputString);
+
+outputString = "Input:  ";
+Console.Write(outputString);
+
+string inputString = Console.ReadLine();
+
+Console.WriteLine("Output: Jeffery Epstein still did not kill himself, " + inputString + "!");
+
