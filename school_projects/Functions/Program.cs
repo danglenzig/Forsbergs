2f54a13d3630a1a3a63434e9851e4049a3f8a506
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-09-24 15:54:28 +0200

    ...

---
+++ school_projects/Functions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+SomeFunction();
+AnotherFunction();
+
+void SomeFunction()
+{
+    Console.WriteLine("Hello World! My name is SomeFunction()");
+}
+
+void AnotherFunction(){}
