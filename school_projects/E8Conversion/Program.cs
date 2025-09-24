d26a54576040938300f563cf3129f41c36c1b74f
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-09-24 13:01:14 +0200

    Casting & conversion exercise

---
+++ school_projects/E8Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Globalization;
+
+// use periods as decimal points, not commas...
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+Console.WriteLine("Output: Give me a number.");
+string numberString = Console.ReadLine();
+
+double numberAsDouble = Convert.ToDouble(numberString);
+
+Console.WriteLine(numberAsDouble);
+
+int numberAsInt = Convert.ToInt32(numberAsDouble);
+
+Console.WriteLine(numberAsInt);
