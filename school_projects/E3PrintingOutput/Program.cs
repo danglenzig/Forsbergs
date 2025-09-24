ece776061a1e29cabd0956374cdecab64003e19b
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-09-24 11:02:11 +0200

    Comment exercise

---
+++ school_projects/E3PrintingOutput/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("Welcome to C# Basics!");
+Console.WriteLine("It's easy to print output...");
+Console.WriteLine("...To the console!");
