dd0aa80a5902c24857d9447ce512ba3c5f25d7fc
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-10-01 13:34:35 +0200

    More exercises

---
+++ school_projects_W2/Week2_E3_1_FunctionPuzzle/Program.cs
@@ -0,0 +1,49 @@
+﻿void F1()
+{
+    // prints A
+    Console.Write("A");
+}
+
+void F2()
+{
+    // prints BA
+    Console.Write("B");
+    F1();
+}
+
+void F3()
+{
+    // prints A-BA
+    F1();
+    Console.Write("-");
+    F2();
+}
+
+void F4()
+{
+    // prints -A
+    Console.Write("-");
+    F1();
+}
+
+/*
+F1();
+Console.WriteLine("\n");
+
+F2();
+Console.WriteLine("\n");
+
+F3();
+Console.WriteLine("\n");
+
+F4();
+Console.WriteLine("\n");
+*/
+
+F2();
+F3();
+F2();
+F1();
+F4();
+F4();
+F3();
---
+++ school_projects_W2/Week2_E3_3_StackCall/Program.cs
@@ -0,0 +1,13 @@
+﻿int timer = 5;
+
+void CountDown()
+{
+    Console.WriteLine(timer);
+    timer -= 1;
+    if (timer >= 0)
+    {
+        CountDown();
+    }
+}
+
+CountDown();
