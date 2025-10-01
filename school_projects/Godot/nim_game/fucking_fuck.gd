68332f9b8b73135f49637bfc99f05fe4f6ea7e56
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-10-01 09:36:11 +0200

    Archived W1 stuff

--- school_projects/Godot/nim_game/fucking_fuck.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-
-
-
-func _ready() -> void:
-	while true:
-		await get_tree().create_timer(0.1).timeout
-		print(randi_range(0,4))
--- school_projects/training_exercises_24sep2025/MinutesToSeconds/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Globalization;
-Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-
-Console.Write("Minutes: ");
-string inputStr = Console.ReadLine();
-double minutesDouble = Convert.ToDouble(inputStr);
-Console.WriteLine("Seconds: " + minutesDouble * 60.0);
