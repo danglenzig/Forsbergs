cbe8fc7f990a0edf239eedacdbe0ae150bc1cd38
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-10-09 18:06:21 +0200

    Inheritance

---
+++ WEEK_3_OOP_BASICS/Inheritance_Ex01/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Animal
+{
+    
+    public string name;
+    public int age;
+
+    public Animal()
+    {
+        name = "unknown";
+        age = -1;
+    }
+    
+    public Animal(string _name)
+    {
+        name = _name;
+        age = -1;
+    }
+
+    public Animal(string _name, int _age)
+    {
+        name = _name;
+        age = _age;
+    }
+    
+    public virtual void Speak()
+    {
+        Console.WriteLine("Generic animal sound");
+    }
+}
+public class Dog : Animal
+{
+    
+    public string breed;
+    
+    public override void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal buddy = new Dog();
+        buddy.Speak();
+    }
+}
