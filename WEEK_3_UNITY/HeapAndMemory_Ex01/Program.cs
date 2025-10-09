fc7ef05ca578983b9b3b84863480134526117a45
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-10-09 10:25:44 +0200

    progress

---
+++ WEEK_3_UNITY/HeapAndMemory_Ex01/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Hello");
+
+/*
+Person p1 = new Person();
+p1.name = "Alice";
+
+Person p2 = new Person();
+p2.name = "Bob";
+
+Console.WriteLine(p1.name + " " + p2.name);
+Console.WriteLine(p1.name == p2.name);
+
+public class Person
+{
+    public string name;
+    
+}
+*/
