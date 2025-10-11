03e8199476f7fc9c7d5ac9c700ab0cdd407e5ad4
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-10-11 13:36:40 +0200

    Interfaces, Properties, and Encapsulation

---
+++ WEEK_3_OOP_BASICS/Interfaces_Ex01/Program.cs
@@ -0,0 +1,55 @@
+﻿interface ISwimable
+{
+    void Swim();
+}
+
+interface IDrawable
+{
+    void Draw();
+}
+
+class Fish:ISwimable
+{
+    public string fishName;
+    public Fish(string _fishName) {fishName = _fishName;}
+    public void Swim() => Console.WriteLine("What the hell is 'water'?");
+}
+
+class Circle : IDrawable
+{
+    public int radius;
+    public Circle(int _radius) {radius = _radius;}
+    public void Draw() => Console.WriteLine("I'm a Circle");
+}
+
+class Rectangle : IDrawable
+{
+    public int width;
+    public int height;
+    public Rectangle(int _width, int _height) {width = _width; height = _height;}
+    public void Draw() => Console.WriteLine("I'm a Rectangle");
+}
+
+class Triangle : IDrawable
+{
+    public int width;
+    public int height;
+    public Triangle(int _width, int _height) {width = _width; height = _height;}
+    public void Draw() => Console.WriteLine("I'm a Triangle");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Fish f = new Fish("Bob");
+        f.Swim();
+        
+        Triangle tr = new Triangle(10, 20);
+        Circle c = new Circle(20);
+        Rectangle r = new Rectangle(10, 20);
+        tr.Draw();
+        c.Draw();
+        r.Draw();
+    }
+}
---
+++ WEEK_3_OOP_BASICS/PropertiesAndEncapsulation_Ex01/Program.cs
@@ -0,0 +1,155 @@
+﻿class Player
+{
+    private string name;
+    private int health;
+    private int level;
+
+    public Player(string _name, int _level)
+    {
+        name = _name;
+        level = _level;
+    }
+    
+    public int Health
+    {
+        get =>health;
+        set
+        {
+            if (value < 0) health = 0;
+            if (value > 100) health = 100;
+            else health = value;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+    }
+
+    public void Heal(int amount)
+    {
+        Health += amount;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"Health: {health}");
+    }
+
+    public void SetHealth(int _health)
+    {
+        Health = _health;
+    }
+    
+}
+
+class ShopItem
+{
+    public string Name;
+    public int Price;
+
+    public bool IsExpensive
+    {
+        get => Price > 100;
+    }
+
+    public ShopItem(string _name, int _price)
+    {
+        Name = _name;
+        Price = _price;
+    }
+}
+
+class Car
+{
+    private int fuel;
+    
+    public string Brand;
+    public int Fuel
+    {
+        get => fuel;
+        set
+        {
+            if (value < 0) fuel = 0;
+            else fuel = value;
+        }
+    }
+
+    public bool IsEmpty
+    {
+        get => fuel <= 0;
+    }
+
+    public Car(string _brand, int _fuel)
+    {
+        Brand = _brand;
+        Fuel = _fuel;
+    }
+
+    public void ReduceFuel(int _amount)
+    {
+        Fuel -= _amount;
+    }
+
+    public void IncreaseFuel(int _amount)
+    {
+        Fuel += _amount;
+    }
+
+    
+    
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Player player = new Player("bob", 2);
+        
+        player.SetHealth(100);
+        player.PrintHealth();
+        Console.WriteLine();
+        
+        player.TakeDamage(60);
+        player.PrintHealth(); // should print "Health: 40"
+        Console.WriteLine();
+        
+        player.TakeDamage(60);
+        player.PrintHealth(); // should print "Health: 0"
+        Console.WriteLine();
+        
+        player.Heal(20);
+        player.PrintHealth(); // should print "Health: 20
+        Console.WriteLine();
+        
+        Console.WriteLine("--------------------------------------");
+        
+        ShopItem cheapItem = new ShopItem("Pepperoni", 10);
+        ShopItem expensiveItem = new ShopItem("Caviar", 200);
+        
+        Console.WriteLine($"{cheapItem.Name} costs {cheapItem.Price}. Is expensive? {cheapItem.IsExpensive}");
+        Console.WriteLine($"{expensiveItem} costs {expensiveItem.Price}. Is expensive? {expensiveItem.IsExpensive}");
+        
+        Console.WriteLine("--------------------------------------");
+        
+        Car car = new Car("Corvette", 100);
+        Console.WriteLine($"{car.Brand} has {car.Fuel} litres. Is empty? {car.IsEmpty}");
+        Console.WriteLine();
+        
+        car.ReduceFuel(60);
+        Console.WriteLine($"{car.Brand} has {car.Fuel} litres. Is empty? {car.IsEmpty}");
+        Console.WriteLine();
+        
+        car.ReduceFuel(60);
+        Console.WriteLine($"{car.Brand} has {car.Fuel} litres. Is empty? {car.IsEmpty}");
+        Console.WriteLine();
+        
+        car.IncreaseFuel(20);
+        Console.WriteLine($"{car.Brand} has {car.Fuel} litres. Is empty? {car.IsEmpty}");
+        Console.WriteLine();
+        
+        
+    }
+}
